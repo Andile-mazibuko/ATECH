f7355133a94801456dd8e892c39372def25b1fc1
Andile-mazibuko <andilelethumusa@gmail.com>
2022-08-14 14:52:16 +0200

    Added the item card

--- dashboard/script.js
+++ dashboard/script.js
@@ -3,29 +3,30 @@
 	{
 		let sidebar = document.getElementById("side-bar");
 		let  arrbtn = document.getElementById("arrow-btn");
-		let paragraph = document.getElementById("content");
-		let navbar = document.getElementById("nav-bar");
+		let navbar = document.getElementById("nav-bar"); 
+		navbar.classList.toggle("close-navbar");
 		sidebar.classList.toggle("close-side-bar");
 		arrbtn.classList.toggle("close-side-bar-btn");
-		paragraph.classList.toggle("hide-paragraph");
-		navbar.classList.toggle("close-navbar");
+		
+		
 	}
 	function enableDarkMode()
 	{
+		let productArea = document.getElementById("product");
 		let body = document.getElementById("body");
 		let sidebar = document.getElementById("side-bar");
 		let  arrbtn = document.getElementById("arrow-btn");
-		//let navbar = document.getElementById("nav-bar"); 
+		let navbar = document.getElementById("nav-bar"); 
+		productArea.classList.toggle("product-dark");
 		body.classList.toggle("dark-mode");
-		//navbar.classList.toggle("dark-navbar");
+		navbar.classList.toggle("dark-navbar");
 		sidebar.classList.toggle("dark-mode-sidebar");
 		
 	}
-	function numberOfItems(){
+	function addItemOnCart(){
 		let number = 0;
 		number++;
 		
-		let span = document.getElementById("items");
-		span.innerHTML = ""+number;
+		let span = document.getElementById("items").innerHTML = ""+number;
 		return number;
 	}
