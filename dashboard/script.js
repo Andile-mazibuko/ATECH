8b4fccc5cd0949f3860e587bf6b84aed531e1d61
Andile-mazibuko <andilelethumusa@gmail.com>
2022-08-30 10:05:03 +0200

    Modified tesing pages

--- dashboard/script.js
+++ dashboard/script.js
@@ -2,14 +2,15 @@
 	function closeSideBar()
 	{
 		let sidebar = document.getElementById("side-bar");
-		let  arrbtn = document.getElementById("arrow-btn");
+		let arrbtn = document.getElementById("arrow-btn");
 		let navbar = document.getElementById("nav-bar"); 		
 		let productArea = document.getElementById("product-area");
+		let span = document.getElementById("a");//testing purposes
 		navbar.classList.toggle("close-navbar");
 		sidebar.classList.toggle("close-side-bar");
 		arrbtn.classList.toggle("close-side-bar-btn");
 		productArea.classList.toggle("close-productArea");
-		
+		span.classList.toggle("invisible-sidecontent");
 		
 	}
 	function enableDarkMode()
