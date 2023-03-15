985de89f2cb6a0e4f2cd5aea8fdf6dc43c1f7086
Andile <andilelethumusa@gmail.com>
2023-03-15 19:45:55 +0200

    a

--- Atehch Web App/build/generated-sources/ap-source-output/atech/entities/ac/za/Product_.java
+++ Atehch Web App/build/generated-sources/ap-source-output/atech/entities/ac/za/Product_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-10-24T16:01:53")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-03-15T19:35:59")
 @StaticMetamodel(Product.class)
 public class Product_ { 
 
